2540d2ad1d11a183a8b0eda92d3e2f2cb5a2de44
Matthew <elijah2895@hotmail.com>
2021-03-16 19:04:43 -0400

    commit tree f569267569eb963d9f509ab18f32488fb78b1bbe

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:index]
+  
+  # The resources way of routing
+  #resources :posts, only: [:index, :new, :create]
+
+  get '/posts', to: 'posts#index', as: 'new'
 end
